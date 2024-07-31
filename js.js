a4938103b79f3e83d5efee69f90298500a228ecf
Raphael Lorenzo <159261495+RaphaelLorenzo777@users.noreply.github.com>
2024-07-31 12:59:30 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,23 @@
+function mimagem(){
+    document.getElementById("conteudo").innerHTML ="<P>O centro de Amsterdam é o coração pulsante da cidade, combinando uma mistura vibrante de charme histórico e dinamismo urbano</P>";
+      document.getElementById("imagem").innerHTML = "<img src='centroamsterdã.jpg'>"
+ 
+}
+ 
+function mimagem2(){
+    document.getElementById("conteudo").innerHTML ="<P>A praça mais inportante de Amsterdam, onde o Pálacio Real domina.</P>";
+    document.getElementById("imagem").innerHTML = "<img src='palaciopraçadam.jpg'>"
+ 
+}
+ 
+function mimagem3(){
+    document.getElementById("conteudo").innerHTML ="<P>Deslumbre a incrivel cidade de Amsterdam.</P>";
+     document.getElementById("imagem").innerHTML = "<img src='Amsterdãparonamica.jpg'>"
+ 
+}
+ 
+function mimagem4(){
+    document.getElementById("conteudo").innerHTML ="<P>Explore o legado de Vincent van Gogh no Museu Van Gogh em Amsterdam, onde você pode admirar suas obras-primas como 'Os Girassóis' e 'A Noite Estrelada', além de mergulhar na fascinante jornada do artista.</P>";
+     document.getElementById("imagem").innerHTML = "<img src='museuvangogh.jpg'>"
+ 
+}
